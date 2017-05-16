9419cf39d3f22284e41618df7a0b5480a9c85be0
atm0016 <atm0016@example.com>
2017-05-16 11:45:21 +0530

    05_16 opperater_php_task

---
+++ basic_operater_task.php
@@ -0,0 +1,27 @@
+<?php
+  echo 7 ."<br>";
+  $name = "yoshiki";
+  $country = "Japan";
+  $age = 21;
+  $corse = "php";
+  echo "my name is ".$name.". I came from ".$country.".<br><br>";
+
+  echo "My age is ".$age.". I came here in MISAO for lerning".$corse.".<br><br>";
+
+  echo 7 + 8 ."<br>";
+  echo 10 - 5 ."<br>";
+  echo 15 / 3 ."<br>";
+  echo 17 * 2 ."<br>";
+  echo 9 % 4 ."<br><br>";
+
+  $a = 7;
+  echo $a++ ."<br><br>";
+  $a = 7;
+  echo $a-- ."<br><br>";
+
+  $a = 5;
+  echo $a++ + ++$a + $a++ + 13 + $a++ ."<br><br>";
+
+  $a = 7;
+  echo $a-- - $a-- - --$a - 7 ."<br><br>";
+ ?>
