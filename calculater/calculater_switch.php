0e15bd3eeb4d8f421c51658073ad989adcdbeed0
atm0016 <atm0016@example.com>
2017-05-17 14:55:48 +0530

    calculater by php

---
+++ calculater/calculater_switch.php
@@ -0,0 +1,41 @@
+<?php
+  $num1 = $_GET["num1"];
+  $num2 = $_GET["num2"];
+  $opperater = $_GET["opperater"];
+  $result;
+
+  switch ($opperater) {
+    case '+':
+    $result = $num1 + $num2;
+    echo "result: ".$result ;
+      break;
+
+    case '-':
+    $result = $num1 - $num2;
+    echo "result: ".$result ;
+      break;
+
+    case '*':
+    $result = $num1 * $num2;
+    echo "result: ".$result ;
+      break;
+
+    case '/':
+    $result = $num1 / $num2;
+    echo "result: ".$result ;
+        break;
+
+    case '%':
+    $result = $num1 % $num2;
+    echo "result: ".$result ;
+      break;
+
+    default:
+    echo "error";
+      break;
+  }
+
+  else if ($opperater == "%") {
+
+  }
+ ?>
---
+++ calculater/index_if.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>calculater</title>
+  </head>
+  <body>
+    <h1>CALCULATER</h1>
+    <form class="" action="calculater_if.php" method="get">
+      <table>
+        <tr>
+          <td>
+            <label for="name1">First-Number</label>
+          </td>
+          <td>
+            <input type="text" name="num1" value=""><br>
+          </td>
+        </tr>
+          <td>
+            <label for="opperater">Opperater</label>
+          </td>
+          <td>
+            <input type="text" name="opperater" value=""><br>
+          </td>
+        <tr>
+            <td>
+              <label for="num2">Second-Number</label>
+            </td>
+            <td>
+              <input type="text" name="num2" value=""><br>
+            </td>
+        </tr>
+        <td colspan="2">
+          <button type="submit" name="button">result</button>
+        </td>
+      </table>
+    </form>
+  </body>
+</html>
